dfb1db239c53c404f45c4c9ee2b51cf7c41757ab
Hussien Talha <113939669+Hussien-Talha@users.noreply.github.com>
2023-10-04 10:57:56 +0300

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,32 +1,33 @@
 // Get the access token from the URL
-const urlParams = new URLSearchParams(window.location.search);
+const urlParams = new URLSearchParams(window.location.hash.substring(1));
 const accessToken = urlParams.get('access_token');
+const scope = urlParams.get('scope');
 
-// Fetch currently playing track from Spotify API
-fetch('https://api.spotify.com/v1/me/player/currently-playing', {
-  headers: {
-    'Authorization': 'Bearer ' + accessToken
-  }
-})
-  .then(response => response.json())
-  .then(data => {
-    if (data.item) {
-      const trackName = data.item.name;
-      const artistName = data.item.artists[0].name;
-
-      document.getElementById('track-name').textContent = trackName;
-      document.getElementById('artist-name').textContent = artistName;
-    } else {
-      document.getElementById('track-name').textContent = 'No track currently playing';
-    }
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-
-// ... previous code ...
-
-// Play button click event
+// Check if the user has granted the user-read-currently-playing scope
+if (scope && scope.includes('user-read-currently-playing')) {
+    // Fetch currently playing track from Spotify API
+    fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.item) {
+          const trackName = data.item.name;
+          const artistName = data.item.artists[0].name;
+  
+          document.getElementById('track-name').textContent = trackName;
+          document.getElementById('artist-name').textContent = artistName;
+        } else {
+          document.getElementById('track-name').textContent = 'No track currently playing';
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  
+    // Play button click event
 document.getElementById('play-btn').addEventListener('click', () => {
     fetch('https://api.spotify.com/v1/me/player/play', {
       method: 'PUT',
@@ -65,4 +66,10 @@ document.getElementById('play-btn').addEventListener('click', () => {
         console.error('Error:', error);
       });
   });
+  
+
+  } else {
+    // The user has not granted the necessary permission
+    console.log('Permission to access currently playing track not granted.');
+  }
   
